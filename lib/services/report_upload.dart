cc7642a09953da6f225312173f3d9bbfdbd3430d
gemechis-elias <gemcs.elias@gmail.com>
2024-04-28 23:34:45 +0300

    [Fix]: Tarrif and Level :alien:

--- lib/services/report_upload.dart
+++ lib/services/report_upload.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
 import 'package:transport_app/main.dart';
 import 'package:transport_app/models/report.dart';
-import 'package:http/http.dart' as http;
+
+import '../models/update_model.dart';
 
 class ReportService {
   static const String baseUrl =
@@ -12,12 +14,16 @@ class ReportService {
     await Hive.openBox<String>(tokenHive);
     final String? token = Hive.box<String>(tokenHive).get('token');
     await Hive.close();
-    
+    final station_box = await Hive.openBox<StationInfo>('station');
+    final String? station_id = station_box.get('station_id')?.id;
+
     try {
       print('======= Uploading report:  ======');
       Map<String, dynamic> data = {
+        "station_id": station_id,
         "uploaded_by": report.name,
         "no_of_ticket": report.amount,
+        "total_service_fee": "100",
         "date": report.date,
         "plate_no": report.plate,
         "validation": "ok"
